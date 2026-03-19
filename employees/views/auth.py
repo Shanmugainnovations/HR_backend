3ba469b8192cd4ddc76a87d274dd8c98026657ac
Parthiban-backend-dev <parthipan3121461@gmail.com>
2026-03-19 15:04:29 +0530

    update timezone

--- employees/views/auth.py
+++ employees/views/auth.py
@@ -4,6 +4,7 @@ from user_agents import parse
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
